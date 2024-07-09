ef5b6b7f6ae10e6956685c1ba25af4774d5bf22d
Paul Chavard <pro@paul.chavard.net>
2024-07-09 18:14:48 +0200

    fix datetime component spec

--- spec/components/editable_champ/datetime_component/datetime_component_spec.rb
+++ spec/components/editable_champ/datetime_component/datetime_component_spec.rb
@@ -1,4 +1,7 @@
 describe EditableChamp::DatetimeComponent, type: :component do
+  let(:procedure) { create(:procedure, types_de_champ_public: [{ type: :datetime, stable_id: 99 }]) }
+  let(:dossier) { create(:dossier, procedure:) }
+
   let(:component) {
     described_class.new(form: instance_double(ActionView::Helpers::FormBuilder, object_name: "dossier[champs_public_attributes]"), champ:)
   }
@@ -8,19 +11,19 @@ describe EditableChamp::DatetimeComponent, type: :component do
     subject { component.formatted_value_for_datetime_locale }
 
     context 'when the value is nil' do
-      let(:champ) { Champs::DatetimeChamp.new(value: nil, dossier: create(:dossier), type_de_champ: create(:type_de_champ_datetime)) }
+      let(:champ) { Champs::DatetimeChamp.new(value: nil, dossier:, stable_id: 99) }
 
       it { is_expected.to be_nil }
     end
 
     context 'when the value is not a valid datetime' do
-      let(:champ) { Champs::DatetimeChamp.new(value: 'invalid', dossier: create(:dossier), type_de_champ: create(:type_de_champ_datetime)) }
+      let(:champ) { Champs::DatetimeChamp.new(value: 'invalid', dossier:, stable_id: 99) }
 
       it { is_expected.to be_nil }
     end
 
     context 'when the value is a valid datetime' do
-      let(:champ) { Champs::DatetimeChamp.new(value: '2020-01-01T00:00:00+01:00', dossier: create(:dossier), type_de_champ: create(:type_de_champ_datetime)) }
+      let(:champ) { Champs::DatetimeChamp.new(value: '2020-01-01T00:00:00+01:00', dossier:, stable_id: 99) }
 
       it { is_expected.to eq('2020-01-01T00:00') }
     end
